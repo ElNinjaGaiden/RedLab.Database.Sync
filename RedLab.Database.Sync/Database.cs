6bdae80ebc2aae7f59dba074e2fafc732d87b430
ElNinjaGaiden <chinopb@gmail.com>
2014-05-20 12:13:53 -0600

    Pushing files

---
+++ RedLab.Database.Sync/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO; 
+
+namespace RedLab.Database.Sync
+{
+    internal class Database
+    {
+        internal int Sync(string checkOutFolder, string foldersToSync, string connectionString)
+        {
+            var syncedFiles = 0;
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                try
+                {
+                    var _foldersToSync = foldersToSync.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var subFolder in _foldersToSync)
+                    {
+                        var filesToSync = ResolveSubFolderFiles(Path.Combine(checkOutFolder, subFolder));
+
+                        foreach (var file in filesToSync)
+                        {
+                            var query = File.ReadAllText(file);
+
+                            using (SqlCommand command = new SqlCommand(query, con))
+                            {
+                                command.ExecuteNonQuery();
+                                syncedFiles++;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            return syncedFiles;
+        }
+
+        internal List<string> ResolveSubFolderFiles(string folderPath)
+        {
+            var fileNames = new List<string>();
+
+            if (Directory.Exists(folderPath))
+            {
+                fileNames.AddRange(Directory.EnumerateFiles(folderPath, "*.sql"));
+            }
+
+            return fileNames;
+        }
+    }
+}
---
+++ RedLab.Database.Sync/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedLab.Database.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 3)
+            {
+                var checkOutFolder = args[0];
+                var foldersToSync = args[1];
+                var connectionString = args[2];
+
+                Console.WriteLine("Checkout folder: {0}", checkOutFolder);
+                Console.WriteLine("Subfolders to sync: {0}", foldersToSync);
+                Console.WriteLine("Connection string: {0}", connectionString);
+
+                var totalSyncedFiles = new Database().Sync(checkOutFolder, foldersToSync, connectionString);
+                Console.WriteLine("Total synced files: {0}", totalSyncedFiles); 
+            }
+            else
+            {
+                Console.WriteLine("No arguments supplied");
+            }
+
+        }
+    }
+}
